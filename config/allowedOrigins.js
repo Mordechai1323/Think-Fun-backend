26edd8d0524bee3744df4ce18b37dbd5fd6e70e5
Mordechai1323 <112948082+Mordechai1323@users.noreply.github.com>
2024-01-23 22:48:11 +0200

    added domain to cors policy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://jocular-muffin-880358.netlify.app",
   "https://think-fun.online",
+  'https://tictactoe-mz.netlify.app'
 ];
 
 module.exports = allowedOrigins;
